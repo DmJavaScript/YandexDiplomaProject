697b5071ffd7d6b7d44b85170f8cb9413fd3fe74
Dmitry Kovegin <dmitry.kovegin@gmail.com>
2021-03-25 22:10:48 +0500

    add api-proxy

--- src/js/news-api.js
+++ src/js/news-api.js
@@ -2,7 +2,7 @@ import {fromDate, today} from './constants.js';
 
 export class NewsApi {
   getNews (inputRequest) {
-    return fetch(`https://newsapi.org/v2/everything?q=${inputRequest}&pageSize=100&language=ru&${fromDate}&${today}&apiKey=a77a12e2e4484b4fb5cc12d192f94b00`, {
+    return fetch(`https://nomoreparties.co/news/v2/everything?q=${inputRequest}&pageSize=100&language=ru&${fromDate}&${today}&apiKey=a77a12e2e4484b4fb5cc12d192f94b00`, {
       method: 'GET'
     })
     .then((res) => {
